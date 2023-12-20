9dc386abbdef5807f4336e39764d6c04c09ef549
DesarrolloJMn <jmora061483@gmail.com>
2023-12-20 17:23:35 -0500

    include

---
+++ resources/views/shared/dialog.blade.php
@@ -0,0 +1,195 @@
+<div id="egreso" class="container">
+    <input type="hidden" id="caja_id" value="{{isset($caja)?$caja->id:''}}">
+    <div class="mb-3">
+        <label class="form-label" for="unidad_medida">
+            Concepto                
+        </label>
+        <input type="text" name="detallepago"  class="form-control"
+        id="concepto">
+    </div>                 
+    <div class="mb-3">
+        <label class="form-label" for="unidad_medida">
+            Valor                 
+        </label>
+        <input type="text" name="valor"  class="form-control"
+        id="valor">
+    </div>            
+</div>     
+<div id="existencias" class="container">
+    <div class="card mb-4">                
+        <div class="card-body">
+            <div class="card mb-4">
+                <div class="card-header">
+                    Datos de <span id="tipo_ex"></span>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="mb-3">
+                                <label class="form-label" for="codigo">
+                                    Codigo
+                                </label>
+                                <span id="codigo_ex"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="mb-3">
+                                <label class="form-label" for="codigo">
+                                    Nombre
+                                </label>
+                                <span id="nombre_ex"></span>
+                            </div>
+                        </div>                                
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">
+                    Datos de inventario
+                </div>
+                <div class="card-body">        
+                    <form action="">
+                        <input type="hidden" name="materiaprima_id" id="materiaprima_id">
+                        <div class="mb-3">
+                            <label class="form-label" for="codigo">
+                                Fecha
+                            </label>
+                            <input class="form-control" type="date" value="{{date('Y-m-d')}}" name="fecha" id="fecha_ex">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="codigo">
+                                Cantidad
+                            </label>
+                            <input type="text" class="form-control" name="catidad" id="cantidad_ex">
+                        </div>                
+                    </form>
+                </div>
+            </div>
+        </div>                   
+</div>
+<div id="ingredientes" class="container">
+    <div class="card mb-4">                
+        <div class="card-body">
+            <div class="card mb-4">
+                <div class="card-header">
+                    Datos de producto
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="mb-3">
+                                <label class="form-label" for="codigo">
+                                    Codigo
+                                </label>
+                                <span id="codigo"></span>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="mb-3">
+                                <label class="form-label" for="codigo">
+                                    Nombre
+                                </label>
+                                <span id="nombre"></span>
+                            </div>
+                        </div>                          
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-4">
+                <div class="card-header">
+                    Datos de ingredientes
+                </div>
+                <div class="card-body">        
+                    <form action="">         
+                        <input type="hidden" id="ingrediente_id">                       
+                        <input type="hidden" id="materia_prima_id">
+                        <div class="mb-3">
+                            <label class="form-label" for="codigo">
+                                Materia prima
+                            </label>
+                            <input class="form-control" type="text"  name="ingrediente" 
+                            id="ingrediente">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="cantidad">
+                                Cantidad
+                            </label>
+                            <input type="text" class="form-control"value="1" name="cantidad" id="cantidad">
+                        </div>                                   
+                    </form>
+                </div>
+            </div>
+        </div>                                   
+</div>
+<div id="DetalleOrden" class="container">
+    <div class="card mb-4">                
+        <div class="card-body">                    
+            <form action="">         
+                <input type="hidden" value="{{isset($orden_id)?$orden_id:0}}" id="orden_id">
+                <input type="hidden" id="ordendetalle_id">                                               
+                <input type="hidden" id="producto_id">                  
+                <div class="row" >
+                    <div class="col-7">
+                        <div class="mb-3">
+                            <label class="form-label" for="cantidad">
+                                Cantidad
+                            </label>
+                            <input type="text" class="form-control" value="1" name="cantidad" id="cantidadDetalleOrden">
+                        </div>                
+                        <div class="mb-3">
+                            <label class="form-label" for="codigo">
+                                Detalle
+                            </label>
+                            <input class="form-control" type="text"  name="detalleOrden" 
+                            id="detalleOrden">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="unidad_medida">
+                                Valor unitario
+                            </label>
+                            <input class="form-control" type="text"  name="ValorUnitarioDetalleOrden" 
+                            id="ValorUnitarioDetalleOrden">
+                        </div>                                                       
+                    </div>
+                    <div class="col-5">
+                        <img id="producto_img" src="" alt="">
+
+                    </div>
+                    
+                </div>                             
+                
+            </form>
+            
+        </div> 
+    </div>                                  
+</div>
+<div id="formasPagos" class="container">
+    <div class="mb-3">
+        <label class="form-label" for="categoria">
+            Forma pago                
+        </label>
+        @if(isset($forma_pago))
+        <select type="text" name="forma_pago" class="form-select"
+         id="forma_pago">
+         <option value="">seleccione una forma pago</option>
+         @foreach($forma_pago as $item)
+         <option value="{{$item->id}}">{{$item->nombre}}</option>
+         @endforeach
+        </select>            
+        @endif  
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="unidad_medida">
+            Detalle pago                
+        </label>
+        <input type="text" name="detallepago"  class="form-control"
+        id="detallepago">
+    </div>                 
+    <div class="mb-3">
+        <label class="form-label" for="unidad_medida">
+            Valor recibido                
+        </label>
+        <input type="text" name="valorRecibido"  class="form-control"
+        id="valorRecibido">
+    </div>            
+</div>
---
+++ resources/views/shared/menu.blade.php
@@ -0,0 +1,49 @@
+
+<div class="nav">
+    <div class="sb-sidenav-menu-heading">Aplicaciones</div>
+    <a class="nav-link" href="{{url('/existencias')}}">
+        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+        Inventario
+    </a>
+   <!--<a class="nav-link" href="{{url('/')}}">
+        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+        Alquiler
+    </a>-->
+    <a class="nav-link" href="{{url('/ordenservicio')}}">
+        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+        Orden de servicio
+    </a>                            
+    <a class="nav-link" href="{{url('/cuentascobrar')}}">
+        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+        Cartera
+    </a>      
+    <a class="nav-link" href="{{url('/pagos')}}">
+        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+        Pagos
+    </a>      
+    <div class="sb-sidenav-menu-heading">Administracion del sistema</div>
+    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+        Administrador
+        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+    </a>
+    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+        <nav class="sb-sidenav-menu-nested nav">                                                                     
+            <a class="nav-link" href="{{url('/formapagos')}}">Formas de pagos</a>
+            <a class="nav-link" href="{{url('/configuracion')}}">Configuracion</a>                                    
+            <a class="nav-link" href="{{url('/impresoras')}}">Impresoras</a>
+            <a class="nav-link" href="{{url('/categorias')}}">Categorias</a>
+            <a class="nav-link" href="{{url('/unidad_medida')}}">Unidades de medida</a>
+            <a class="nav-link" href="{{url('/materiaprimas')}}">Materia primas</a>
+            <a class="nav-link" href="{{url('/cajas')}}">Cajas</a>
+            <a class="nav-link" href="{{url('/cabañas')}}">Cabañas</a>
+            <a class="nav-link" href="{{url('/productos')}}">Productos</a>
+            <a class="nav-link" href="{{url('/empleados')}}">Empleados</a>
+            <a class="nav-link" href="{{url('/empresas')}}">Empresas</a>
+            <a class="nav-link" href="{{url('/clientes')}}">Clientes</a>
+            <a class="nav-link" href="{{url('/roles')}}">Roles</a>
+            <a class="nav-link" href="{{url('/impuestos')}}">Impuestos</a> 
+            <a class="nav-link" href="{{url('/observaciones')}}">Observaciones</a> 
+        </nav>
+    </div>                         
+</div>
